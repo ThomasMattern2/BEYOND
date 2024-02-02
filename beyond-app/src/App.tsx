64f203ab1c76c3d379c5fbc30127f15407e8e9d7
aleabalda <alessandro.baldassar@ucalgary.ca>
2024-02-02 11:58:47 -0700

    created react app and added header

---
+++ beyond-app/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./BEYOND (1).jpg"
+
+function App() {
+  return (
+    <>
+    <div id="header"><img id="title" src={logo} alt="title"></img></div>
+    </>
+  );
+}
+
+export default App;
