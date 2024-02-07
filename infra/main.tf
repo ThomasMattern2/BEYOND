b614eb85a6bf8460fdad8f47a2d8efdc47042623
thomasmattern2 <thomasmattern1@gmail.com>
2024-02-07 18:40:58 +0000

    lambda function template

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,21 @@
+resource "aws_lambda_function" "name" {
+  s3_bucket     = aws_s3_bucket.name.bucket
+  s3_key        = "name/artifact.zip"
+  role          = aws_iam_role.lambda.arn
+  function_name = "nameoffunction"
+  handler       = "main.lambda_handler"
+  runtime       = "python3.9"
+}
 
+resource "aws_lambda_function_url" "get_url" {
+  function_name      = "nameoffunction
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = [""]
+    allow_methods     = ["get/post"]
+    allow_headers     = ["*"]
+    expose_headers    = [""]
+  }
+}
