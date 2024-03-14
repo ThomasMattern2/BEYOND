0358ca42e34e7c6d0ae96d9ea3c99a1e15626cbf
carson-may <carson.may@ucalgary.ca>
2024-03-13 18:55:19 -0600

    added new functions to main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -66,6 +66,39 @@ resource "aws_iam_policy" "logs" {
 EOF
 }
 
+
+resource "aws_dynamodb_table" "db" {
+  name           = "beyond-users"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+
+  hash_key = "email"
+
+  attribute {
+    name = "email"
+    type = "S"    # type string
+  }
+  # (More attributes can be added as data is added to the database) 
+}
+
+resource "aws_dynamodb_table" "db-objects" {
+  name           = "beyond-objects"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+
+  hash_key = "NGC"
+
+  attribute {
+    name = "NGC"
+    type = "N"    # type number
+  }
+  # (More attributes can be added as data is added to the database) 
+}
+
+# get-user resources
+
 resource "aws_iam_role" "get-user" {
   name                = "iam-for-lambda-get-user"
   assume_role_policy  = <<EOF
@@ -83,22 +116,6 @@ resource "aws_iam_role" "get-user" {
 ]
 }
 EOF
-}
-
-
-resource "aws_dynamodb_table" "db" {
-  name           = "beyond-users"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
-
-  hash_key = "email"
-
-  attribute {
-    name = "email"
-    type = "S"    # type string
-  }
-  # (More attributes can be added as data is added to the database) 
 }
 
 resource "aws_lambda_function" "get-user" {
@@ -251,4 +268,178 @@ resource "aws_lambda_function_url" "create-user-url" {
 
 output "create-lambda_url" {
   value = aws_lambda_function_url.create-user-url.function_url
+}
+
+
+# create-object resources
+
+resource "aws_iam_role" "create-object" {
+  name                = "iam-for-lambda-create-object"
+  assume_role_policy  = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+  {
+    "Action": "sts:AssumeRole",
+    "Principal": {
+      "Service": "lambda.amazonaws.com"
+    },
+    "Effect": "Allow",
+    "Sid": ""
+  }
+]
+}
+EOF
+}
+
+resource "aws_lambda_function" "create-object" {
+  role          = aws_iam_role.create-object.arn
+  function_name = "create-object"
+  handler       = local.lambda_handler
+  filename      = "../functions/create-object/dist/create-object.zip"
+
+  runtime = "python3.12"
+}
+
+resource "aws_iam_role_policy_attachment" "create-object_logs" {
+  role       = aws_iam_role.create-object.name
+  policy_arn = aws_iam_policy.logs.arn
+}
+
+resource "aws_iam_role_policy_attachment" "create-object_dynamo" {
+  role       = aws_iam_role.create-object.name
+  policy_arn = aws_iam_policy.dynamo.arn
+}
+
+resource "aws_lambda_function_url" "create-object-url" {
+  function_name      = aws_lambda_function.create-object.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["POST"]
+    allow_headers     = ["*"]
+    expose_headers    = ["keep-alive", "date"]
+  }
+}
+
+output "create-object-lambda_url" {
+  value = aws_lambda_function_url.create-object-url.function_url
+}
+
+# get-object resources
+
+resource "aws_iam_role" "get-object" {
+  name                = "iam-for-lambda-get-object"
+  assume_role_policy  = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+  {
+    "Action": "sts:AssumeRole",
+    "Principal": {
+      "Service": "lambda.amazonaws.com"
+    },
+    "Effect": "Allow",
+    "Sid": ""
+  }
+]
+}
+EOF
+}
+
+resource "aws_lambda_function" "get-object" {
+  role          = aws_iam_role.get-object.arn
+  function_name = "get-object"
+  handler       = local.lambda_handler
+  filename      = "../functions/get-object/dist/get-object.zip"
+
+  runtime = "python3.12"
+}
+
+
+resource "aws_iam_role_policy_attachment" "get-object_logs" {
+  role       = aws_iam_role.get-object.name
+  policy_arn = aws_iam_policy.logs.arn
+}
+
+resource "aws_iam_role_policy_attachment" "get-object_dynamo" {
+  role       = aws_iam_role.get-object.name
+  policy_arn = aws_iam_policy.dynamo.arn
+}
+
+resource "aws_lambda_function_url" "get-object-url" {
+  function_name      = aws_lambda_function.get-object.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["GET"]
+    allow_headers     = ["*"]
+    expose_headers    = ["keep-alive", "date"]
+  }
+}
+
+output "get-object_url" {
+  value = aws_lambda_function_url.get-object-url.function_url
+}
+
+# get-all-objects resources
+
+resource "aws_iam_role" "get-all-objects" {
+  name                = "iam-for-lambda-get-all-objects"
+  assume_role_policy  = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+  {
+    "Action": "sts:AssumeRole",
+    "Principal": {
+      "Service": "lambda.amazonaws.com"
+    },
+    "Effect": "Allow",
+    "Sid": ""
+  }
+]
+}
+EOF
+}
+
+resource "aws_lambda_function" "get-all-objects" {
+  role          = aws_iam_role.get-all-objects.arn
+  function_name = "get-all-objects"
+  handler       = local.lambda_handler
+  filename      = "../functions/get-all-objects/dist/get-all-objects.zip"
+
+  runtime = "python3.12"
+}
+
+
+resource "aws_iam_role_policy_attachment" "get-all-objects_logs" {
+  role       = aws_iam_role.get-all-objects.name
+  policy_arn = aws_iam_policy.logs.arn
+}
+
+resource "aws_iam_role_policy_attachment" "get-all-objects_dynamo" {
+  role       = aws_iam_role.get-all-objects.name
+  policy_arn = aws_iam_policy.dynamo.arn
+}
+
+resource "aws_lambda_function_url" "get-all-objects-url" {
+  function_name      = aws_lambda_function.get-all-objects.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["GET"]
+    allow_headers     = ["*"]
+    expose_headers    = ["keep-alive", "date"]
+  }
+}
+
+output "get-all-objects_url" {
+  value = aws_lambda_function_url.get-all-objects-url.function_url
 }
