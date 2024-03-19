6e1921ba71f8ddd81244e259d0eeabcafa44b106
carson-may <carson.may@ucalgary.ca>
2024-03-19 15:19:36 -0600

    updated get-all function

--- functions/get-all-objects/main.py
+++ functions/get-all-objects/main.py
@@ -16,7 +16,8 @@ def get_all_objects():
     try:
         # Scan the table for all items.
         response = table.scan()
-        return response['Items']
+        # Return the list of items with only the desired columns.
+        return [{k: v for k, v in item.items() if k not in ["name", "type", "collection"]} for item in response["Items"]]
     except Exception as e:
         return {"error": str(e)}
 
