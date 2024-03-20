330cf9cc1ed6581652b033ba34b5d21b81203f09
carson-may <carson.may@ucalgary.ca>
2024-03-19 20:46:57 -0600

    deleted commented code

--- run_juni_build.sh
+++ run_juni_build.sh
@@ -1,28 +1,3 @@
-# #!/bin/bash
-
-# # List of directories to run 'juni build' in
-# directories=(
-#     "functions/add-favourite"
-#     "functions/create-object"
-#     "functions/create-user"
-#     "functions/delete-favourite"
-#     "functions/delete-user"
-#     "functions/edit-user"
-#     "functions/get-all-objects"
-#     "functions/get-favourites"
-#     "functions/get-object"
-#     "functions/get-user"
-#     # Add more directories as needed
-# )
-
-# # Loop through each directory and run 'juni build'
-# for dir in "${directories[@]}"; do
-#     echo "Running 'juni build' in $dir..."
-#     cd "$dir" && juni build
-# done
-
-# echo "Completed running 'juni build' in all directories."
-
 #!/bin/bash
 
 # Base directory containing the function directories
